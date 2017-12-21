a7bba57abc11c621ab7ab97c21675228a361a72f
Don <donfreiday@gmail.com>
2017-12-21 09:18:24 -0500

    Changed camera resolution to 1280x720

--- Camera-Server/camera_pi.py
+++ Camera-Server/camera_pi.py
@@ -10,7 +10,7 @@ class Camera(BaseCamera):
         with picamera.PiCamera() as camera:
             # let camera warm up
             time.sleep(2)
-
+            camera.resolution = (1280, 720)
             stream = io.BytesIO()
             for foo in camera.capture_continuous(stream, 'jpeg',
                                                  use_video_port=True):
