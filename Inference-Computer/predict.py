3354004e3646a5637c442ce5c6d4defd010b9e33
Don <donfreiday@gmail.com>
2017-12-24 12:05:34 -0500

    Only print selected detections.

--- Inference-Computer/predict.py
+++ Inference-Computer/predict.py
@@ -8,7 +8,7 @@ import requests
 from PIL import Image
 import numpy as np
 
-options = {"model": "cfg/yolo-voc.cfg", "load": "bin/yolo-voc.weights", "threshold": 0.1}
+options = {"model": "cfg/yolo-voc.cfg", "load": "bin/yolo-voc.weights", "threshold": 0.15}
 
 tfnet = TFNet(options)
 
@@ -24,8 +24,8 @@ while True:
     #imgcv = cv2.imread('./sample/bird.png')
     result = tfnet.return_predict(curr_img_cv2)
     for detection in result:
-        print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + ": " + detection['label'] + " detected")
         if detection['label'] == 'bird' or detection['label'] == 'cat' or detection['label'] == 'person':
+            print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + ": " + detection['label'] + " detected")
             curr_img.save('{0}/{1}.jpg'.format(detection['label'], datetime.now().strftime('%Y-%m-%d %H.%M.%S')))
 
     time.sleep(3)
